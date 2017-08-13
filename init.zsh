70a1bc1eb629702ef47d1faa638261b1da18eac8
Asen Lekov <asenlekoff@gmail.com>
2017-08-14 00:13:30 +0300

    Added init.zsh

---
+++ init.zsh
@@ -0,0 +1,12 @@
+SOURCE=${0%/*}
+
+# Aliases
+source $SOURCE/aliases/aliases.zsh
+
+source $SOURCE/lib/key-bindings.zsh
+
+# Various functions
+source $SOURCE/functions/functions.zsh
+
+# Colors
+source $SOURCE/colors/config.zsh
