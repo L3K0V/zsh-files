c029812180115f6fe41c00ff1f3a3bf0f280fe7c
Asen Lekov <asenlekoff@gmail.com>
2018-07-10 19:15:02 +0300

    Update aliases
    Clear gradle caches with a glance

--- aliases/aliases.zsh
+++ aliases/aliases.zsh
@@ -40,6 +40,7 @@ alias webp2png="find . -name '*.webp' | parallel -eta dwebp {} -o {.}.png"
 alias compresspngs="find . -name '*.png' -exec pngquant --ext .png --force 256 {} \;"
 alias deletepngs="find . -type f -iname \*.png -delete"
 alias deletewebps="find . -type f -iname \*.webp -delete"
+alias gradleclean="rm -rf $HOME/.gradle/caches/"
 
 # Empty the Trash on all mounted volumes and the main HDD
 # Also, clear Apple’s System Logs to improve shell startup speed
